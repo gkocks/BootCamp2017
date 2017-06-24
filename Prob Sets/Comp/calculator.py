34550fedadba4713fc46d83a092540d98b637905
gkocks <geoffrey_kocks@brown.edu>
2017-06-24 11:56:27 -0500

    Add files via upload

---
+++ Prob Sets/Comp/calculator.py
@@ -0,0 +1,16 @@
+#calculator.py
+"""This is used for Problem 3 of the Standard Library lab for OSM."""
+
+import math
+
+def add(a,b):
+    return a+b
+
+def root(a):
+    return math.sqrt(a)
+
+def product(a,b):
+    return a*b
+
+if __name__ == "__main__":
+    pass
